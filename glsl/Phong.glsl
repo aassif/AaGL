3b333c2c330d1c156559295778a921f7024e9109
Aa! <aassif.benassarou@univ-reims.fr>
2013-03-22 11:43:47 +0100

    Gestion des inclusions multiples.

--- glsl/Phong.glsl
+++ glsl/Phong.glsl
@@ -1,3 +1,6 @@
+#ifndef AA_PHONG
+#define AA_PHONG
+
 uniform float aa_phong_ambient   = 0.0;
 uniform float aa_phong_diffuse   = 1.0;
 uniform float aa_phong_specular  = 0.5;
@@ -6,11 +9,13 @@ uniform float aa_phong_shininess = 16;
 const vec3 AA_PHONG_EYE   = normalize (vec3 (0, 0, 1));
 const vec3 AA_PHONG_LIGHT = normalize (vec3 (0, 0, 1));
 
-vec4 aa_phong (vec3 n, vec4 c)
+vec4 aa_phong (vec3 n, vec4 c, float white = 1)
 {
   vec3  normal   = normalize (n);
   float diffuse  = max (0, dot (AA_PHONG_LIGHT, normal));
   float specular = pow (max (0, dot (reflect (-AA_PHONG_LIGHT, normal), AA_PHONG_EYE)), aa_phong_shininess);
-  return vec4 (c.rgb * (aa_phong_ambient + diffuse * aa_phong_diffuse + specular * aa_phong_specular), c.a);
+  return vec4 (c.rgb * (aa_phong_ambient + diffuse * aa_phong_diffuse) + vec3 (white * specular * aa_phong_specular), c.a);
 }
+
+#endif // AA_PHONG
 
--- glsl/core/Fragment.glsl
+++ glsl/core/Fragment.glsl
@@ -1,3 +1,6 @@
+#ifndef AA_FRAGMENT
+#define AA_FRAGMENT
+
 #extension GL_ARB_shading_language_include : require
 
 #include "/Aa/Phong"
@@ -5,10 +8,12 @@
 in vec3 aa_vertex_normal;
 in vec4 aa_vertex_color;
 
-layout (location = 0) out vec4 aa_fragment_color;
+out layout (location = 0) vec4 aa_fragment_color;
 
 void aa_gl_core ()
 {
   aa_fragment_color = aa_phong (aa_vertex_normal, aa_vertex_color);
 }
+
+#endif // AA_FRAGMENT
 
