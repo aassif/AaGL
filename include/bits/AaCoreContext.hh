dcb7cc6f624e720bd4f4c9005629d9c9cfabb5a9
Aa! <aassif@gmail.com>
2013-06-04 02:10:55 +0200

    Simplification de Aa::GL::CoreContext.

--- include/bits/AaCoreContext.hh
+++ include/bits/AaCoreContext.hh
@@ -16,20 +16,17 @@ namespace Aa
         }
 
       private:
-        Program * m_program;
-        mat4      m_modelview;
-        mat4      m_projection;
-        vec2      m_depth_range;
-        bool      m_moving;
+        mat4 m_modelview;
+        mat4 m_projection;
+        vec2 m_depth_range;
+        bool m_moving;
 
       public:
         inline
-        CoreContext (Program    * program,
-                     const mat4 & modelview,
+        CoreContext (const mat4 & modelview,
                      const mat4 & projection,
                      const vec2 & depth_range,
                      bool         moving = false) :
-          m_program     (program),
           m_modelview   (modelview),
           m_projection  (projection),
           m_depth_range (depth_range),
@@ -37,25 +34,10 @@ namespace Aa
         {
         }
 
-        inline Program    * program     () const {return m_program;}
         inline const mat4 & modelview   () const {return m_modelview;}
         inline const mat4 & projection  () const {return m_projection;}
         inline const vec2 & depth_range () const {return m_depth_range;}
         inline bool         is_moving   () const {return m_moving;}
-
-#if 0
-        inline
-        void use () const
-        {
-          m_program->use ();
-          m_program->set<GLfloat, 4, 4> ("aa_gl_modelview",          m_modelview);
-          m_program->set<GLfloat, 4, 4> ("aa_gl_modelview_inverse",  m_modelview.inv ());
-          m_program->set<GLfloat, 4, 4> ("aa_gl_projection",         m_projection);
-          m_program->set<GLfloat, 4, 4> ("aa_gl_projection_inverse", m_projection.inv ());
-          m_program->set<GLfloat, 3, 3> ("aa_gl_normal_matrix",      NormalMatrix (m_modelview));
-          m_program->set<GLfloat, 2>    ("aa_gl_depth_range",        m_depth_range);
-        }
-#endif
     };
 
   } // namespace GL
