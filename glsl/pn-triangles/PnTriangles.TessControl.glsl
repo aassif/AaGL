c8b65a12570103436b417e2963e54898e5a3e222
Aa! <aassif.benassarou@univ-reims.fr>
2013-07-19 13:28:44 +0200

    Ré-écriture des "Curved PN Triangles".

--- glsl/pn-triangles/PnTriangles.TessControl.glsl
+++ glsl/pn-triangles/PnTriangles.TessControl.glsl
@@ -2,8 +2,6 @@
 
 #extension GL_ARB_shading_language_include : require
 
-#include "/Aa/PnTriangles"
- 
 uniform float aa_pn_triangles_levels;
  
 in layout (location = 0) vec3 aa_vertex_normal [];
@@ -13,54 +11,16 @@ layout (vertices = 3) out;
 
 out vec3    aa_control_normal [3];
 out vec4    aa_control_color  [3];
-out PnPatch aa_control_patch  [3];
- 
-float wij (int i, int j)
-{
- return dot (gl_in[j].gl_Position.xyz - gl_in[i].gl_Position.xyz, aa_vertex_normal[i]);
-}
- 
-float vij (int i, int j)
-{
- vec3 Pj_minus_Pi = gl_in[j].gl_Position.xyz
-                  - gl_in[i].gl_Position.xyz;
- vec3 Ni_plus_Nj  = aa_vertex_normal[i]+aa_vertex_normal[j];
- return 2.0 * dot (Pj_minus_Pi, Ni_plus_Nj) / dot (Pj_minus_Pi, Pj_minus_Pi);
-}
  
 void main ()
 {
- gl_out            [gl_InvocationID].gl_Position = gl_in            [gl_InvocationID].gl_Position;
- aa_control_normal [gl_InvocationID]             = aa_vertex_normal [gl_InvocationID];
- aa_control_color  [gl_InvocationID]             = aa_vertex_color  [gl_InvocationID];
+  gl_out            [gl_InvocationID].gl_Position = gl_in            [gl_InvocationID].gl_Position;
+  aa_control_normal [gl_InvocationID]             = aa_vertex_normal [gl_InvocationID];
+  aa_control_color  [gl_InvocationID]             = aa_vertex_color  [gl_InvocationID];
  
- float P0 = gl_in[0].gl_Position[gl_InvocationID];
- float P1 = gl_in[1].gl_Position[gl_InvocationID];
- float P2 = gl_in[2].gl_Position[gl_InvocationID];
- float N0 = aa_vertex_normal[0][gl_InvocationID];
- float N1 = aa_vertex_normal[1][gl_InvocationID];
- float N2 = aa_vertex_normal[2][gl_InvocationID];
- 
- aa_control_patch[gl_InvocationID].b210 = (2.0*P0 + P1 - wij(0,1)*N0)/3.0;
- aa_control_patch[gl_InvocationID].b120 = (2.0*P1 + P0 - wij(1,0)*N1)/3.0;
- aa_control_patch[gl_InvocationID].b021 = (2.0*P1 + P2 - wij(1,2)*N1)/3.0;
- aa_control_patch[gl_InvocationID].b012 = (2.0*P2 + P1 - wij(2,1)*N2)/3.0;
- aa_control_patch[gl_InvocationID].b102 = (2.0*P2 + P0 - wij(2,0)*N2)/3.0;
- aa_control_patch[gl_InvocationID].b201 = (2.0*P0 + P2 - wij(0,2)*N0)/3.0;
-
- float E = (aa_control_patch[gl_InvocationID].b210
-          + aa_control_patch[gl_InvocationID].b120
-          + aa_control_patch[gl_InvocationID].b021
-          + aa_control_patch[gl_InvocationID].b012
-          + aa_control_patch[gl_InvocationID].b102
-          + aa_control_patch[gl_InvocationID].b201) / 6.0;
-
- float V = (P0 + P1 + P2)/3.0;
- aa_control_patch[gl_InvocationID].b111 = E + (E - V)*0.5;
- aa_control_patch[gl_InvocationID].n110 = N0+N1-vij(0,1)*(P1-P0);
- aa_control_patch[gl_InvocationID].n011 = N1+N2-vij(1,2)*(P2-P1);
- aa_control_patch[gl_InvocationID].n101 = N2+N0-vij(2,0)*(P0-P2);
- 
- gl_TessLevelOuter [gl_InvocationID] = aa_pn_triangles_levels;
- gl_TessLevelInner [0]               = aa_pn_triangles_levels;
+  gl_TessLevelOuter [0] = aa_pn_triangles_levels;
+  gl_TessLevelOuter [1] = aa_pn_triangles_levels;
+  gl_TessLevelOuter [2] = aa_pn_triangles_levels;
+  gl_TessLevelInner [0] = aa_pn_triangles_levels;
 }
+
--- glsl/pn-triangles/PnTriangles.TessEval.glsl
+++ glsl/pn-triangles/PnTriangles.TessEval.glsl
@@ -3,16 +3,14 @@
 #extension GL_ARB_shading_language_include : require
 
 #include "/Aa/Core"
-#include "/Aa/PnTriangles"
 
 uniform float aa_pn_triangles_weight;
 
 layout (triangles, equal_spacing, ccw) in;
 
-in vec3    aa_control_normal [];
-in vec4    aa_control_color  [];
-in vec2    aa_control_tex2d  [];
-in PnPatch aa_control_patch  [];
+in vec3 aa_control_normal [];
+in vec4 aa_control_color  [];
+in vec2 aa_control_tex2d  [];
 
 out vec4  aa_eval_position;
 out vec3  aa_eval_normal;
@@ -20,69 +18,79 @@ out vec4  aa_eval_color;
 out vec2  aa_eval_tex2d;
 out float aa_eval_depth;
 
-#define b300    gl_in[0].gl_Position.xyz
-#define b030    gl_in[1].gl_Position.xyz
-#define b003    gl_in[2].gl_Position.xyz
-#define n200    aa_control_normal[0]
-#define n020    aa_control_normal[1]
-#define n002    aa_control_normal[2]
-#define uvw     gl_TessCoord
-
-void main()
+void main ()
 {
-  float u = uvw [0], u2 = u * u, u3 = u2 * u;
-  float v = uvw [1], v2 = v * v, v3 = v2 * v;
-  float w = uvw [2], w2 = w * w, w3 = w2 * w;
+  vec3 p1 = gl_in[0].gl_Position.xyz;
+  vec3 p2 = gl_in[1].gl_Position.xyz;
+  vec3 p3 = gl_in[2].gl_Position.xyz;
 
-  vec3 b210 = vec3 (aa_control_patch[0].b210, aa_control_patch[1].b210, aa_control_patch[2].b210);
-  vec3 b120 = vec3 (aa_control_patch[0].b120, aa_control_patch[1].b120, aa_control_patch[2].b120);
-  vec3 b021 = vec3 (aa_control_patch[0].b021, aa_control_patch[1].b021, aa_control_patch[2].b021);
-  vec3 b012 = vec3 (aa_control_patch[0].b012, aa_control_patch[1].b012, aa_control_patch[2].b012);
-  vec3 b102 = vec3 (aa_control_patch[0].b102, aa_control_patch[1].b102, aa_control_patch[2].b102);
-  vec3 b201 = vec3 (aa_control_patch[0].b201, aa_control_patch[1].b201, aa_control_patch[2].b201);
-  vec3 b111 = vec3 (aa_control_patch[0].b111, aa_control_patch[1].b111, aa_control_patch[2].b111);
+  vec3 n1 = aa_control_normal[0];
+  vec3 n2 = aa_control_normal[1];
+  vec3 n3 = aa_control_normal[2];
 
-  vec3 n110 = normalize (vec3 (aa_control_patch[0].n110, aa_control_patch[1].n110, aa_control_patch[2].n110));
-  vec3 n011 = normalize (vec3 (aa_control_patch[0].n011, aa_control_patch[1].n011, aa_control_patch[2].n011));
-  vec3 n101 = normalize (vec3 (aa_control_patch[0].n101, aa_control_patch[1].n101, aa_control_patch[2].n101));
+  float u = gl_TessCoord.x;
+  float v = gl_TessCoord.y;
+  float w = gl_TessCoord.z;
 
-  vec4 color = gl_TessCoord[2] * aa_control_color[0]
-             + gl_TessCoord[0] * aa_control_color[1]
-             + gl_TessCoord[1] * aa_control_color[2];
+  vec3 b300 = p1;
+  vec3 b030 = p2;
+  vec3 b003 = p3;
 
-  vec3 n1 = gl_TessCoord[2] * aa_control_normal[0]
-          + gl_TessCoord[0] * aa_control_normal[1]
-          + gl_TessCoord[1] * aa_control_normal[2];
+  float w12 = dot (p2 - p1, n1);
+  float w21 = dot (p1 - p2, n2);
+  float w13 = dot (p3 - p1, n1);
+  float w31 = dot (p1 - p3, n3);
+  float w23 = dot (p3 - p2, n2);
+  float w32 = dot (p2 - p3, n3);
 
-  vec3 n2 = n200 * w2
-          + n020 * u2
-          + n002 * v2
-          + n110 * w * u
-          + n011 * u * v
-          + n101 * w * v;
+  vec3 b210 = (2.0 * p1 + p2 - w12 * n1) / 3.0;
+  vec3 b120 = (2.0 * p2 + p1 - w21 * n2) / 3.0;
+  vec3 b021 = (2.0 * p2 + p3 - w23 * n2) / 3.0;
+  vec3 b012 = (2.0 * p3 + p2 - w32 * n3) / 3.0;
+  vec3 b102 = (2.0 * p3 + p1 - w31 * n3) / 3.0;
+  vec3 b201 = (2.0 * p1 + p3 - w13 * n1) / 3.0;
 
-  vec3 normal = mix (n1, n2, aa_pn_triangles_weight);
+  vec3 b111a = (b210 + b120 + b021 + b012 + b102 + b201) / 6.0;
+  vec3 b111b = (p1 + p2 + p3) / 3.0;
+  vec3 b111 = mix (b111a, b111b, aa_pn_triangles_weight);
 
-  vec3 p1 = gl_TessCoord[2] * b300
-          + gl_TessCoord[0] * b030
-          + gl_TessCoord[1] * b003;
+  vec3 p = 1.0 * b300 * w * w * w
+         + 1.0 * b030 * u * u * u
+         + 1.0 * b003 * v * v * v
+         + 3.0 * b210 * u * w * w
+         + 3.0 * b120 * u * u * w
+         + 3.0 * b201 * v * w * w
+         + 3.0 * b021 * u * u * v
+         + 3.0 * b102 * v * v * w
+         + 3.0 * b012 * u * v * v
+         + 6.0 * b111 * u * v * w;
 
-  vec3 p2 = b300 * w3
-          + b030 * u3
-          + b003 * v3
-          + b210 * 3.0 * w2 * u
-          + b120 * 3.0 * u2 * w
-          + b201 * 3.0 * w2 * v
-          + b021 * 3.0 * u2 * v
-          + b102 * 3.0 * v2 * w
-          + b012 * 3.0 * v2 * u
-          + b111 * 6.0 * u * v * w;
+  float v12 = 2.0 * dot (p2 - p1, n1 + n2) / dot (p2 - p1, p2 - p1);
+  float v23 = 2.0 * dot (p3 - p2, n2 + n3) / dot (p3 - p2, p3 - p2);
+  float v31 = 2.0 * dot (p1 - p3, n3 + n1) / dot (p1 - p3, p1 - p3);
 
-  vec3 position = mix (p1, p2, aa_pn_triangles_weight);
+  vec3 n200 = n1;
+  vec3 n020 = n2;
+  vec3 n002 = n3;
+  vec3 n110 = normalize (n1 + n2 - v12 * (p2 - p1));
+  vec3 n011 = normalize (n2 + n3 - v23 * (p3 - p2));
+  vec3 n101 = normalize (n3 + n1 - v31 * (p1 - p3));
 
-  aa_eval_position = vec4 (position, 1.0);
+  vec4 color = w * aa_control_color[0]
+             + u * aa_control_color[1]
+             + v * aa_control_color[2];
+
+  vec3 normal = n200 * w * w
+              + n020 * u * u
+              + n002 * v * v
+              + n110 * w * u
+              + n011 * u * v
+              + n101 * w * v;
+
+  aa_eval_position = vec4 (p, 1.0);
   aa_eval_normal   = normal;
   aa_eval_color    = color;
   aa_eval_depth    = aa_gl_depth       (aa_eval_position);
   gl_Position      = aa_gl_projection * aa_eval_position;
 }
+
